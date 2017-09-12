06f83549cee108b743d5246e8849090d062e59a6
Frew A Negash <zulufanke@yahoo.com>
2017-09-12 12:42:08 -0400

    checkpoint attempt submitted

---
+++ src/products/products.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import Products from "../Products"
+import Comment from "../Comment"
+
+class Products extends Component {
+  render () {
+    return (
+      <div/>
+    )
+  }
+}
+
+export default Products
