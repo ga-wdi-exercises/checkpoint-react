e327c2695cd165e2903eee6df92d0cb817b635bf
Wilner Charles <wilnercharles3@gmail.com>
2017-12-13 09:20:38 -0500

    setup

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './styles/index.css';
 import App from './components/App/App';
 
 import contacts from "./contacts.json";
-
+import {
+  BrowserRouter as Router
+} from 'react-router-dom'
 ReactDOM.render(<App />, document.getElementById('root'));
