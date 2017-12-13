c8684648524d61194e556c7ad092d999c2d6d770
Steven Connors <steven.connors53@gmail.com>
2017-12-13 09:59:34 -0500

    checkpoint turn in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import Contact from "../Contact/Contact";
+import ContactList from "../ContactList/ContactList";
+import Header from "../Header/Header";
+import NewContact from "../NewContact/NewContact";
+import { Route, Link, Redirect, Switch } from "react-router-dom";
 
 class App extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+        contacts: []
+      }
+      this.handleNewContact = this.handleNewContact.bind(this)
+    }
+
+  handleNewContact(newContact) {
+    let tempContact = this.state.contacts
+    tempContact.push(newContact)
+    this.setState({
+      contacts: tempContact
+    })
+    this.props.history.push(`/*/${newContact.symbol}`)
+  }
   render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+    return (
+      <div>
+        <div className="App">
+          <p>app</p>
+        </div>;
+      </div>
+    )
   }
 }
 
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,9 @@
+import { Route, Link, Redirect, Switch } from "react-router-dom";
+import React, { Component } from 'react'
+
+class Contact extends Component{
+
+}
+
+
+export default Contact;
