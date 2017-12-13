955e1dae48f1edc568e5436fd54c83a3aa8798dc
Declan Kurant <Kurant_Declan@bah.com>
2017-12-13 09:57:10 -0500

    finished lab

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom'
+
+import Contact from '../Contact/Contact'
+import ContactList from '../ContactList/ContactList'
+import NewContact from '../NewContact/NewContact'
+import Header from '../Header/Header'
+import Contacts from '../../contacts.json'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = { Contacts: Contacts }
+  }
+
   render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+    return(
+        <Router>
+          <div className="App">
+            <nav>
+              { Header }
+            </nav>
+            <main>
+              <Switch>
+                <Route
+                  path="/"
+                  component= { ContactList }
+                  />
+                <Route
+                  path="/new-contact"
+                  component= { NewContact }
+                />
+              </Switch>
+            </main>
+          </div>
+        </Router>
+      )
+    }
   }
 }
 
 export default App;
+    set the initial state for your component
