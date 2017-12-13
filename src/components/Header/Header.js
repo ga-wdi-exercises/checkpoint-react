9d638a675baabea3a76e1bbba5fea38aa5b388b7
Wilner Charles <wilnercharles3@gmail.com>
2017-12-13 09:59:37 -0500

    Header-contact

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,5 +5,9 @@ class Header extends Component {
   render() {
     return
     <h1> Contact-List <h1>
+    <nav>
+     <Link to="/">Contact</Link>
+     <Link to="/new-contact">New-Contact</Link>
+    </nav>
   }
   export default App;
