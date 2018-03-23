0c4ca156f9204db03ef44bb6300fd7fae4315f4d
Timmy Le <le.timmy94@gmail.com>
2018-03-23 10:00:42 -0400

    need to retake

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ContactList extends Component {
+  render() {
+    return;
+    <div className="contact-list">
+      <p>{this.props.contacts}</p>
+    </div>;
+  }
+}
+
+export default ContactList;
