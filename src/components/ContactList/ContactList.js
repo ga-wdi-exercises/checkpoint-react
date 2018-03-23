e56cc9831a66aa8a4320315dbd06e09c4395aa5f
SAMUEL HAILE <samishken@gmail.com>
2018-03-23 10:00:04 -0400

    initial react checkpoint

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ContactList;
