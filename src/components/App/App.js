9c0ade2522727ae7e299a3de784d9aa9fec6d099
Laura Haft <Lchaft@gmail.com>
2018-03-23 10:00:47 -0400

    failing is fun

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import ContactList from '../ContactList/ContactList'
 import NewContact from '../NewContact/NewContact'
 
