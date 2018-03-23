cfe8fb5d9f6c8a9177147b2c9ac2b0cf37831b1d
Qya Ahmad <raqayyah@msn.com>
2018-03-23 10:01:02 -0400

    components set working on routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: this.props.contacts
+    };
+  }
   render() {
-    return <div className="App">
+    return (
+      <div className="App">
         <p>app</p>
-      </div>;
+      </div>
+    );
   }
 }
 
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="contact">
+        <img />
+        <h3>name</h3>
+        <h4>emailaddress</h4>
+      </div>
+    );
+  }
+}
+
+export default Contact;
+// Your <Contact> component will render a single contact.
+// It should contain an outer
+// <div> with a class of contact and include an
+// <img> for the profile picture,
+// <h3> for the name, and
+// <h4> for the email address.
+// Pass the details in to pass test
