76822848050075afd436b1825a5c433d9c07c28e
Angela Malone <a.malone1193@gmail.com>
2018-03-23 09:59:47 -0400

    lol

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
+import Header from "../Header/Header.js";
+import ContactList from "../ContactList/ContactList.js";
+import Contact from "../Contact/Contact.js";
 
 class App extends Component {
   render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+    this.state = {
+      contacts: this.props.contacts
+    };
+    return (
+      <div className="App">
+        <Header />
+        <ContactList />
+      </div>
+    );
   }
 }
 
---
+++ src/components/NewContact/NewContact.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
+
+class NewContact extends Component {
+  render() {
+    return <h1>New Contact</h1>;
+  }
+}
+
+export default ContactList;
