b1c8f5c1b64fbba1fb2c2a654d5047f781c1559e
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-03-23 10:01:03 -0400

    not working obviously

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom'
 
 class App extends Component {
+  constructor ()  {
+    super ()
+      this.state = {
+        contacts: this.props.contacts
+      }
+  }
+  
   render() {
     return <div className="App">
         <p>app</p>
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+    constructor (props) {
+      super(props)
+  
+      this.state = {
+        contact: this.props.info
+      }
+    }
+  
+    render () {
+      return (
+        <div className='contact'>
+          <img src={this.state.contact.imageUrl} alt={this.state.contact.name} className='contact-image' />
+          <h3>{this.state.contact.name}</h3>
+            <h4>email: {this.state.contact.email}</h4>
+        </div>
+      )
+    }
+  }
+  
+  export default Contact
+  
