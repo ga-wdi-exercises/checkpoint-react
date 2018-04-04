e7b3cbebe8d8fccf360e0aa5aeb5377e89036d3e
SAMUEL HAILE <samishken@gmail.com>
2018-04-04 14:00:13 -0400

    add component defined

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,43 +1,60 @@
 import React, { Component } from 'react'
-//import React, { Component } frimport React, { Component } from 'react'
+// import React, { Component } frimport React, { Component } from 'react'
 import { Link, Switch, Route, Redirect } from 'react-router-dom'   // good  Link Route, Switch, Redirect
-import contacts from "../../contacts.json"
-import Contact from '../Contact/Contact.js'
+import ContactsData from '../../contacts.json'
+// import Contact from '../Contact/Contact.js'
 import ContactList from '../ContactList/ContactList.js'
-import NewContacts from '../NewContact/NewContact.js'
-import Header from '../Header/Header.js' // good
-
+import NewContact from '../NewContact/NewContact.js'
+// import Header from '../Header/Header.js' // good
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      contacts: contacts
-    }
-    console.log(this.state.contacts)
-  }
+ constructor () {
+   super()
+   this.state = {
+     contacts: ContactsData
+   }
+   console.log(this.state.contacts)
+   this.AddContacts = this.AddContacts.bind(this)
+ }
 
-  render () {
-    return (
-      <div>
-        <nav>
-          <h1>Contact Manager</h1>
-          <Link to='/ContactList'>Contacts</Link>
-          <Link to='/ContactList'>Home</Link>
-           {/* <Link to='/contacts'>New Contacts</Link> */}
-        </nav>
-        <main>
-          <Switch>
-          <Route path='/ContactList' component={ContactList} />
-          <Route path='/ContactList' render={() => <ContactList />} /> */}
-             <Route path='/contacts' render={() => <NewContacts />} />
-            <Route path='/contacts' render={() => <Header contacts={this.state.contacts} />} />
-            <Route path='/contacts' render={(props) => <ContactList {...props} contacts={this.state.Contacts} />} />
-            <Route path='/*' render={() => <Redirect to='/contacts' />} /> 
-          </Switch> 
-        </main> 
-      </div>
-    )
-  }
+//  AddContacts (event) {
+//    event.preventDefault()
+//    console.log(this.refs)
+//    // this.setState({
+//    //   NewContact: this.state.contacts.push
+//    // })
+//    // contacts.push(contact, i)
+//  }
+
+ AddContacts () {
+   // let array = this.state.contacts
+   // contact.push(contact)
+   // this.setState({
+   //   contacts: contact
+   // })
+    console.log('this function worked')
+ }
+
+
+ render () {
+   return (
+     <div>
+      <nav className='nav'>
+        <h1>Contact Manager</h1>
+        {/* <Link to='/ContactList'>Contacts </Link> */}
+        <Link to='/ContactList'> Home </Link>
+        <Link to='/NewContact'> New Contacts </Link>
+      </nav>
+      <main>
+        <Switch>
+          <Route path='/NewContact' render={() => <NewContact AddContacts={this.AddContacts} />} />
+          <Route path='/contacts' render={() => <ContactList contacts={this.state.contacts} />} />
+          <Route path='/*' render={() => <Redirect to='/contacts' />} />
+        </Switch>
+      </main>
+    </div>
+   )
+ }
 }
 export default App
+
---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                HELLO
+            </div>
+        );
+    }
+}
+
+export default Hello;
