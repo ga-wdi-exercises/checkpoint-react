6a7f1e14ea847377ace5562cfc300f245bda4f13
Qya Ahmad <raqayyah@msn.com>
2018-04-04 14:35:10 -0400

    just starting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Contacts from ./index
+
+
 
 class App extends Component {
-  render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+  constructor (props) {
+    super(props)
+    this.state = {
+      contacts: this.props.contacts
+    }
+  };
+  render () {
+    return <div className='App'>
+      <Header/>
+      <p> <ContactList></p>
+      <p> <NewContactList> </p> 
+      <p> 
+    </div>
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App/App';
+import Router from 'react-router';
+import contacts from './contacts.json';
 
-import contacts from "./contacts.json";
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default Router;
