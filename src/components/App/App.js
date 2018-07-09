d1c515d0e644c497ab0cd35aeda1865fe46e60c2
Daniel Lousa <dlousa92@gmail.com>
2018-07-09 09:59:57 -0400

    checkpoint test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Header from '../Header/Header'
+import {
+  Link,
+  Route
+} from 'react-router-dom'
+import ContactList from '../ContactList/ContactList'
+import NewContact from '../NewContact/NewContact'
+// const contacts = require('../../contacts.json')
 
+console.log(typeof data)
 class App extends Component {
-  render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+  constructor () {
+    super()
+
+    // this.state = {
+    //   contacts: contacts
+    // }
+  }
+  render () {
+    return (
+      <div className='App'>
+        <Header />
+        <Route path='/'
+          component= {ContactList}
+          exact
+        />
+        <Route path='/new-contact'
+          component = {NewContact}
+          exact
+        />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
