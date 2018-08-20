95cd47e69b72bee8e30973b3d5032e0f41e62a9c
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-20 10:13:10 -0400

    checkpoint react, had to restart

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import contacts from "../../contacts.json"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // where contacts is the array of contacts from contacts.json
+      contacts: this.props.contacts
+    }
+
+  }
   render() {
     return <div className="App">
-        <p>app</p>
+        <p>{contacts}</p>
       </div>;
   }
 }
