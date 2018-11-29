35389b9a08306c953c6f3eb1adaa7a6a790af4f3
Lovdeep <lovdeepgahunia1@gmail.com>
2018-11-29 10:11:11 -0500

    checkpoint3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import Contacts from "../Contact/Contact";
+import ContactList from "../ContactList/ContactList";
+import Header from "../Header/Header";
+import Contact from "../Contact/Contact";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: this.props.contacts
+    };
+  }
   render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+    return (
+      <div className="App">
+        <header>
+          <Link to="/">
+            {" "}
+            <h1>idk</h1>
+          </Link>
+          <nav>
+            <Link to="/contact">Contact</Link>
+            <Link to="/new-contact">Contactlist</Link>
+          </nav>
+        </header>
+
+        <main>
+          <Route path="/" component={Header} exact={true} />
+          <Route path="/contact" component={Contact} exact={true} />
+          <Route path="/contactlist" component={ContactList} exact={true} />
+        </main>
+      </div>
+    );
   }
 }
 
---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ContactList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ContactList</h1>
+      </div>
+    );
+  }
+}
+
+export default ContactList;
