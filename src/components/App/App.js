7bc98561dde1590ed0044cbd3034ca3a6d6e76b6
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-11-29 10:11:02 -0500

    react checkpoint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import contacts from "../../contacts.json";
+import NewContact from "../NewContact/NewContact.js"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      contacts: this.props.contacts
+    };
+  }
+
   render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+    return (
+      <div className="App">
+        <header>
+        <Link to="/">
+            <h1>Home</h1>
+            </Link>
+            <Link to="/NewContact">
+            <h1>New Contact</h1>
+            </Link>
+        </header>
+        <main>
+              <Route path="/" exact component={Home}/>
+              <Route path="/NewContact" exact component={NewContact}/>
+        </main>
+      </div>
+    );
   }
 }
 
 export default App;
+
+// Inside index.js, import React Router and pass it to ReactDOM.render() as the root component with <App> as a child component. We've imported the starter contacts for you, pass these in to your <App> component as a prop, contacts.
+
+// Your <App> component should also render a div with a class name of 'App', your <Header> component and then either your <ContactList> or <NewContact> component, depending on the route. If the route is "/" then render the <ContactList> component; if the route is "/new-contact" then render the <NewContact> component;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App/App';
+import { BrowserRouter as Router } from "react-router-dom";
+ 
 
 import contacts from "./contacts.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+      <App contacts={contacts} />
+    </Router>,
+    document.getElementById("root")
+);
+
+// Inside index.js, import React Router and pass it to ReactDOM.render() as the root component with <App> as a child component. We've imported the starter contacts for you, pass these in to your <App> component as a prop, contacts.
