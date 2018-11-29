10a255d2074f2d700a76adaa606f61635925b6f5
“Christina <cfthompson321@gmail.com>
2018-11-29 10:11:02 -0500

    need retake

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: this.props.contacts
+    };
+    this.setContacts = this.setContacts.bind(this);
+  }
   render() {
-    return <div className="App">
+    return (
+      <div className="App">
         <p>app</p>
-      </div>;
+        <main>
+          <Route path="/" exact component={Header} />
+          <Route path="/" exact component={ContactList} />
+        </main>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <img />
+      </div>
+    );
+  }
+}
+export default Contact;
