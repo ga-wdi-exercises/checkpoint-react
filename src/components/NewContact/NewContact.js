2784ea3c9c08c1728c5163b0bb7598cf43fa7b67
Jasmin Vargas <jas.vrgs@gmail.com>
2019-03-12 10:33:50 -0400

    add form to new contact page

--- src/components/NewContact/NewContact.js
+++ src/components/NewContact/NewContact.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react';
 class NewContact extends Component {
     render(){
         return(
+            <div>
             <h1>New Contact Page</h1>
-
-        
-        )
+            <form>
+            <p>Name</p>
+                <input type="text" />
+            <p>Email</p>
+                <input type="Email" />
+            <p>Image</p>
+                <input type="Image" />
+            <button type="submit">Submit</button> 
+            </form>
+            </div>
+        );
     }
 }
 
