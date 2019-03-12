d6a0069e794db18c71f37a8556d9dbc30a0908a4
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-03-12 09:14:26 -0400

    First two test suites passed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+import Header from '../Header/Header'
+import ContactList from '../ContactList/ContactList'
+import NewContact from '../NewContact/NewContact'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = { contacts: props.contacts }
+  }
   render() {
-    return <div className="App">
-        <p>app</p>
-      </div>;
+    return(
+      <div className="App">
+        <Header />
+        <Route path="/" exact component={ContactList}/>
+        <Route path="/new-contact" component={NewContact}/>
+      </div>
+    )
   }
 }
 
