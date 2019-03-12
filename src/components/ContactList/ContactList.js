96705ac064d926325d2ec5065551803cf949b8e9
Leonard Busenah <busenah@gmail.com>
2019-03-12 10:21:12 -0400

    finalizing commit

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+class Results extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            voiceAudioSource: null
+        }
+    }
+
+    return(
+      <div>
+    <h2>Profie Picture</h2>
+    <h3>Name</h3>
+    <h4>email</h4>
+      </div >
+    )
+  }
+
+
+export default Results
