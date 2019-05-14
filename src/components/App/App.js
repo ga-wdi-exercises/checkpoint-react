86a233374bd8412994c074be50de0b950951a152
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-05-14 10:03:50 -0400

    smh

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import Header from "../Header/Header";
 import ContactList from "../ContactList/ContactList";
-
+import newContact from "../NewContact/NewContact";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,7 +10,11 @@ class App extends Component {
       allContacts: props.contacts
     };
   }
-
+  componentDidMount() {
+    this.state.allContacts.map((contact, index) => {
+      return <p key={index}>Name: {contact.name}</p>;
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -23,6 +27,7 @@ class App extends Component {
               <ContactList contacts={this.state.allContacts} />
             )}
           />
+          <Route path="/new-contact" component={newContact} />
         </Switch>
       </div>
     );
---
+++ src/components/NewContact/NewContact.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class NewContact extends Component {
+  render() {
+    return <div>New Contact</div>;
+  }
+}
+
+export default NewContact;
