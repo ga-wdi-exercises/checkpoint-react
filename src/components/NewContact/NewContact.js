89209e6dbb0e37da64ebbf03b36f6fa4ff701791
Ellison Rio <rio.ellison@gmail.com>
2019-06-18 09:17:44 -0400

    added tweety and added starter rcc for components

---
+++ src/components/NewContact/NewContact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewContact extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default NewContact;
