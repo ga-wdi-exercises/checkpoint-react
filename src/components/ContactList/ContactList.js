172e97cc73dd4394439940c0d21319bd0c7ae263
Melika <melika.r@gmail.com>
2019-06-21 15:31:07 -0400

    add ContactList Component, not completed

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ContactList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        contacts: props.contacts
+        };
+    }
+
+    render(){
+        return
+    }
+}
+
+export default ContactList;
