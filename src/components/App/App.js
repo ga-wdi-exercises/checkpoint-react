0fc7a1fd2d5bca84f0a8732805b395e9c51f1119
Victor Johnson <vjohnson22@gmail.com>
2019-08-14 09:25:04 -0400

    header component created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      contacts: props.contacts
+    }
+  }
   render() {
     return <div className="App">
+        <Header/>
         <p>app</p>
       </div>;
   }
