506a994ed39bba9e18d4f543e4304741d41ea87c
COnokala <C.Onokala@gmail.com>
2019-09-24 10:20:00 -0400

    checkpoint

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,12 @@
+import React,  { Component } from "react";
+import "./ContactList.test";
+
+class ContactList extends Component {
+    render() {
+        return (
+            <div className="contact-list">
+                
+            </div>
+        )
+    }
+}
