cbbfbc2cfeb48ca5461fa3e1043c4418c0f91dd9
Tymothy J Hanks <tymjhanks@gmail.com>
2019-09-24 10:21:43 -0400

    Tym react checkpoint work

---
+++ src/components/NewContact/NewContact.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class NewContactList extends Component{
+    constructor(props){
+        super(props)
+
+        this.state ={
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>New Contact</h1>
+            </div>
+        )
+    }
+}
+export default NewContactList
